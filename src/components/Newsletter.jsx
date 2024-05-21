9c71be17465869710e404a63bb43e5c2668098cc
Sam281055 <sl281055@gmail.com>
2024-05-21 14:16:15 -0400

    First commit

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,11 @@
+import Banner from "../shared/Banner";
+
+const Newsletter = () => {
+    return (
+        <div className="md:px-14 p-4 max-w-screen-2xl mx-auto my-12">
+            <Banner heading={"Each student an share their discount code for friends"} subheading={"A simple paragraph is comprised of three major components. the first sentence, which is ofthen a declarative sentence, is called the topic sentence."} btn1={"I have a code"} btn2={""}/>
+        </div>
+    );
+};
+
+export default Newsletter;
